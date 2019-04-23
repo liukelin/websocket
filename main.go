3152dc932cdd185f1b869d1020e4f87b82f500c8
liukelin <liukelin@liukelin.top>
2019-04-23 18:28:49 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 	"websocket/server"
 )
 
-
 // 参数接收
 var Port  *string 	  // TCP监听端口
 var Queue *string	  // 使用消息队列类型 redis/disque
 
 func main(){
-	
 	Port = flag.String("port", "3210", "服务端口端口")
 	Queue = flag.String("queue", "list", "使用消息中间类型: (list or redis//localhost:6379)")
 	flag.Parse()
