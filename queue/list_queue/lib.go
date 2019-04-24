46c061e86461a774ab7cbcd364514ed184af11bc
liukelin <liukelin@liukelin.top>
2019-04-24 16:26:31 +0800

    Update lib.go

--- queue/list_queue/lib.go
+++ queue/list_queue/lib.go
@@ -3,7 +3,7 @@ package list_queue
 /**
   1、使用自带的container/list包 实现本地队列（此为非线程安全，这里使用加锁）
   2、未提供清理功能，长期堆积不消费可能会内存爆炸，这里简单的限制单个list len数量
-  3、附带一个自动清理功能（超过过期时间的元素自动清理，因为是非现场安全这个先不考虑用异步回收器做）
+  3、附带一个自动清理功能（超过过期时间的元素自动清理，因为是非线程安全这个先不考虑用异步回收器做）
   必要实现
   func NewQueue
   func Push
@@ -74,7 +74,7 @@ func (q *Queue) Pop() (interface{}) {
     // 没有直接的pop操作，这两步操作非原子性，因为本类是非线程安全，这里加锁
     defer lock.Unlock()
     lock.Lock()
-	e := q.List.Back()
+    e := q.List.Back()
     if e != nil {
         q.List.Remove(e)
         return e.Value
